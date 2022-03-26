b8870c997428e9cd22f5b5cab353b50c6597ddf6
ifyouinme <ydch0702@gamil.com>
2022-03-26 19:46:01 +0800

    Data

--- 3536.cpp
+++ 3536.cpp
@@ -1,3 +1,13 @@
 #include<iostream>
 #include<iomanip>
+#include<cmath>
 using namespace std;
+
+int main()
+{
+	int n,x;//n为盘数，x为次数
+	cin >> n;
+	x = pow(2, n) - 1;
+	cout << x << endl;
+	return 0;
+}
